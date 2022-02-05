db74dfd18ae84b5a18c67b730c7750d770128633
gingercw <gingercw@gmail.com>
2022-02-05 01:19:46 +0000

    Get player's guess

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 """A number-guessing game."""
 import random
 number = random.randint(1, 100)
+player = input("Welcome to the Guessing Game! Please enter your name: ")
+guess = input(f"Nice to meet you, {player}! I'm thinking of a number between 1 and 100. Guess the number: ")
 
-user_name = input("Welcome to the Guessing Game! Please enter your name: ")
