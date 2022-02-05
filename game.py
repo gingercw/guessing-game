62e558b505cb00a96729b3dba9705d0eb1f8c635
gingercw <gingercw@gmail.com>
2022-02-05 02:44:11 +0000

    Now includes error message for non-integer inputs

--- game.py
+++ game.py
@@ -5,14 +5,20 @@ number = random.randint(1, 100)
 guess = int(input(f"Nice to meet you, {player}! I'm thinking of a number between 1 and 100. Guess the number: "))
 guess_count = 1
 
+
 while True:
-  if guess < 1 or guess > 100:
-      guess = int(input("Whomp whomp. That guess is not between 1-100. Try again: "))
-      guess_count += 1
-  elif guess != number and guess >= 1 and guess <= 100:
-      guess = int(input("The guess is too high. Try again: ")) if guess > number else int(input("The guess is too low. Try again: "))
-      guess_count += 1
-  else:
-    print(f"Great job, {player}! You guessed the number in only {guess_count} tries.")
-    exit()
+  try:
+    if guess < 1 or guess > 100:
+        guess = int(input("Whomp whomp. That guess is not between 1-100. Try again: "))
+        guess_count += 1
+    elif guess != number and guess >= 1 and guess <= 100:
+        guess = int(input("The guess is too high. Try again: ")) if guess > number else int(input("The guess is too low. Try again: "))
+        guess_count += 1
+    else:
+      print(f"Great job, {player}! You guessed the number in only {guess_count} tries.")
+      exit()
+      break
+  except ValueError:
+    guess = int(input("Dude. That was not a number.  Try again...: "))
+    guess_count += 1
 
