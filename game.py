d83c54630e4bf194068b1e1b5a861da0ba320941
gingercw <gingercw@gmail.com>
2022-02-05 03:07:00 +0000

    allows player to try again after non-integer guess

--- game.py
+++ game.py
@@ -2,23 +2,25 @@
 import random
 player = input("Welcome to the Guessing Game! Please enter your name: ")
 number = random.randint(1, 100)
-guess = int(input(f"Nice to meet you, {player}! I'm thinking of a number between 1 and 100. Guess the number: "))
+guess = input(f"Nice to meet you, {player}! I'm thinking of a number between 1 and 100. Guess the number: ")
 guess_count = 1
-
 
 while True:
   try:
+    guess = int(guess)
     if guess < 1 or guess > 100:
-        guess = int(input("Whomp whomp. That guess is not between 1-100. Try again: "))
+        guess = input("Whomp whomp. That guess is not between 1-100. Try again: ")
         guess_count += 1
     elif guess != number and guess >= 1 and guess <= 100:
-        guess = int(input("The guess is too high. Try again: ")) if guess > number else int(input("The guess is too low. Try again: "))
+        guess = input("The guess is too high. Try again: ") if guess > number else input("The guess is too low. Try again: ")
         guess_count += 1
     else:
       print(f"Great job, {player}! You guessed the number in only {guess_count} tries.")
       exit()
       break
   except ValueError:
-    guess = int(input("Dude. That was not a number.  Try again...: "))
+    guess = input("That's not a number... Try again: ")
     guess_count += 1
+
+
 
